b931929918e49e63514a8331e39e406828bf8cb8
tejasKhamkar2211 <tejaskhamkar23@gmail.com>
2025-02-28 21:48:34 +0530

    check

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('!!! nodejs throuhh cicd pipeline  dokyala kat kat!!!');
+ res.send('!!! ya check !!!');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
